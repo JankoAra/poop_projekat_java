42e02f2b58b1d030d5920eda350de45d2aeec885
Janko Arandjelovic <janko.arandjelovic@gmail.com>
2023-07-18 01:10:46 +0200

    zapoceto pravljenje gui glavnog programa

--- poop_projekat_java/src/application/Main.java
+++ poop_projekat_java/src/application/Main.java
@@ -6,6 +6,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -18,27 +19,157 @@ import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.TextAlignment;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+//
+//	private TableView<String[]> table; // Replace String[] with your data model
+//
+//	private GridPane gridPane;
+//	private int rowCount = 2;
+//	private final int COLUMN_COUNT = 26;
+//
+//	// indeksiranje pocinje od 0
+//	public Node getElementOfGrid(GridPane grid, int rowIndex, int colIndex) {
+//		rowIndex++;
+//		colIndex++;
+//		int index = rowIndex * Table.numOfCols + colIndex;
+//		if (index < 0 || index > grid.getChildren().size())
+//			return null;
+//		return grid.getChildren().get(index);
+//	}
+//
+//	public void replaceElementInGrid(GridPane grid, Node newElem, int rowIndex, int colIndex) {
+//		rowIndex++;
+//		colIndex++;
+//		int index = rowIndex * Table.numOfCols + colIndex;
+//		if (index < 0 || index > grid.getChildren().size())
+//			return;
+//		grid.getChildren().set(index, newElem);
+//	}
+//
+//	@Override
+//	public void start(Stage primaryStage) {
+//		primaryStage.setTitle("Excel Program");
+//		Controller cont = new Controller();
+//		String currentDirectory = System.getProperty("user.dir");
+//		System.out.println("Current Directory: " + currentDirectory);
+//		Table t = Parser.loadCSVTable("test1.csv");
+//		Controller.postaviTabelu(t);
+//
+//		// Create menu bar
+//		MenuBar menuBar = new MenuBar();
+//		Menu fileMenu = new Menu("File");
+//		MenuItem newMenuItem = new MenuItem("New Table");
+//		newMenuItem.setOnAction(e -> {
+//			System.out.println("nova");
+//		});
+//		MenuItem loadMenuItem = new MenuItem("Load Table");
+//		fileMenu.getItems().addAll(newMenuItem, loadMenuItem);
+//		menuBar.getMenus().add(fileMenu);
+//
+//		// Create grid pane
+//		gridPane = GUI.populateGrid(t);
+//		ScrollPane sp = new ScrollPane(gridPane);
+//
+//		// Create the main layout
+//		BorderPane root = new BorderPane();
+//
+//		// Create a button for saving the table
+//		Button dodajredbtn = new Button("dodaj red");
+//		dodajredbtn.setOnAction(e -> cont.dodajRed(gridPane));
+//
+//		Button printbtn = new Button("stampaj");
+//		// printbtn.setOnAction(e -> cont.stampaj(gridPane));
+//		printbtn.setOnAction(e -> {
+//			sp.setContent(GUI.populateGrid(t));
+//		});
+//
+//		TextField red = new TextField();
+//		red.setPromptText("red");
+//		TextField kolona = new TextField();
+//		kolona.setPromptText("kolona");
+//		TextField sadrzaj = new TextField();
+//		sadrzaj.setPromptText("sadrzaj");
+//
+//		Button dodajpolje = new Button("dodaj polje");
+//		dodajpolje.setOnAction(e -> cont.dodajPolje(Integer.parseInt(red.getText()), Integer.parseInt(kolona.getText()),
+//				new Cell(sadrzaj.getText())));
+//
+//		Button replacebtn = new Button("replace");
+//		replacebtn.setOnAction(e -> {
+//			Label l = new Label("zamenjeno");
+//			replaceElementInGrid(gridPane, l, Integer.parseInt(red.getText()), Integer.parseInt(kolona.getText()));
+//		});
+//
+//		// Create a VBox to hold the save button
+//		VBox buttonBox = new VBox(10);
+//		buttonBox.getChildren().add(dodajredbtn);
+//		buttonBox.getChildren().add(printbtn);
+//		buttonBox.getChildren().addAll(red, kolona, sadrzaj);
+//		buttonBox.getChildren().add(dodajpolje);
+//		buttonBox.getChildren().add(replacebtn);
+//		buttonBox.setPadding(new Insets(10));
+//
+//		// Add the VBox to the right side of the main layout
+//		root.setRight(buttonBox);
+//		root.setCenter(sp);
+//
+//		// Create the scene and set it on the stage
+//		Scene scene = new Scene(root, 800, 600);
+//		primaryStage.setScene(scene);
+//		primaryStage.show();
+//	}
+//
+////	@Override
+////	public void start(Stage primaryStage) throws IOException {
+////		FXMLLoader loader = new FXMLLoader(getClass().getResource("tabela.fxml"));
+////
+////		Parent root = loader.load();
+////
+////		Scene scene = new Scene(root);
+////		primaryStage.setScene(scene);
+////		primaryStage.setTitle("Excel by Janko");
+////		primaryStage.show();
+////	}
+//
+//	private void saveTable() {
+//		// Implement the logic to save the table in the desired format
+//		System.out.println("Cuvam");
+//	}
+//
+//	public static void main(String[] args) {
+//		launch(args);
+//	}
 
-	private TableView<String[]> table; // Replace String[] with your data model
+	private Table table;
 
-	private GridPane gridPane;
-	private int rowCount = 2;
-	private final int COLUMN_COUNT = 26;
+	// indeksiranje pocinje od 0
+	public Node getElementOfGrid(GridPane grid, int rowIndex, int colIndex) {
+		rowIndex++;
+		colIndex++;
+		int index = rowIndex * Table.numOfCols + colIndex;
+		if (index < 0 || index > grid.getChildren().size())
+			return null;
+		return grid.getChildren().get(index);
+	}
 
-	@Override
-	public void start(Stage primaryStage) {
-		primaryStage.setTitle("Excel Program");
-		Controller cont = new Controller();
-		String currentDirectory = System.getProperty("user.dir");
-		System.out.println("Current Directory: " + currentDirectory);
-		Table t = Parser.loadCSVTable("t.csv");
-		Controller.postaviTabelu(t);
+	public void replaceElementInGrid(GridPane grid, Node newElem, int rowIndex, int colIndex) {
+		rowIndex++;
+		colIndex++;
+		int index = rowIndex * Table.numOfCols + colIndex;
+		if (index < 0 || index > grid.getChildren().size())
+			return;
+		grid.getChildren().set(index, newElem);
+	}
+
+	public Scene makeAndPopulateScene() {
+		// Create the main layout
+		BorderPane root = new BorderPane();
 
 		// Create menu bar
 		MenuBar menuBar = new MenuBar();
@@ -50,50 +181,95 @@ public class Main extends Application {
 		MenuItem loadMenuItem = new MenuItem("Load Table");
 		fileMenu.getItems().addAll(newMenuItem, loadMenuItem);
 		menuBar.getMenus().add(fileMenu);
+		
+		BorderPane menuBarAndOptions = new BorderPane();
+		menuBarAndOptions.setTop(menuBar);
 
-		// Create grid pane
-		gridPane = GUI.populateGrid(t);
-		ScrollPane sp = new ScrollPane(gridPane);
+		// Create table manipulation options
+		FlowPane northMenu = new FlowPane();
+		menuBarAndOptions.setCenter(northMenu);
 
-		// Create the main layout
-		BorderPane root = new BorderPane();
+		// Buttons and fields
+		Button saveBtn = new Button("Save");
+		Button addRowBtn = new Button("Add row");
 
-		// Create a button for saving the table
-		Button saveButton = new Button("dodaj red");
-		saveButton.setOnAction(e -> cont.dodajRed(gridPane));
+		TextField rowIndexField = new TextField();
+		rowIndexField.setPromptText("row");
+		TextField columnIndexField = new TextField();
+		columnIndexField.setPromptText("column");
+		TextField newValueField = new TextField();
+		newValueField.setPromptText("new value");
+		Button changeValueBtn = new Button("Change value");
+		
+		VBox vbox1 = new VBox(saveBtn, addRowBtn);
+		VBox vbox2 = new VBox(rowIndexField, columnIndexField, newValueField, changeValueBtn);
+		vbox1.setPadding(new Insets(5));
+		vbox2.setPadding(new Insets(5));
 
-		Button printbtn = new Button("stampaj");
-		// printbtn.setOnAction(e -> cont.stampaj(gridPane));
-		printbtn.setOnAction(e -> {
-			sp.setContent(GUI.populateGrid(t));
-		});
+		northMenu.getChildren().addAll(vbox1,vbox2);
+		// grid in a scroll pane
+		ScrollPane sp = new ScrollPane(GUI.populateGrid(table));
 
-		TextField red = new TextField();
-		red.setPromptText("red");
-		TextField kolona = new TextField();
-		kolona.setPromptText("kolona");
-		TextField sadrzaj = new TextField();
-		sadrzaj.setPromptText("sadrzaj");
-
-		Button dodajpolje = new Button("dodaj polje");
-		dodajpolje.setOnAction(e -> cont.dodajPolje(Integer.parseInt(red.getText()), Integer.parseInt(kolona.getText()),
-				new Cell(sadrzaj.getText())));
-
-		// Create a VBox to hold the save button
-		VBox buttonBox = new VBox(10);
-		buttonBox.getChildren().add(saveButton);
-		buttonBox.getChildren().add(printbtn);
-		buttonBox.getChildren().addAll(red, kolona, sadrzaj);
-		buttonBox.getChildren().add(dodajpolje);
-		buttonBox.setPadding(new Insets(10));
-
-		// Add the VBox to the right side of the main layout
-		root.setRight(buttonBox);
+		// populate layout and make scene
+		root.setTop(menuBarAndOptions);
 		root.setCenter(sp);
+		Scene scene = new Scene(root,1000,800);
+		return scene;
+	}
+
+	@Override
+	public void start(Stage primaryStage) {
+		primaryStage.setTitle("Excel by JANKO");
+		String currentDirectory = System.getProperty("user.dir");
+		System.out.println("Current Directory: " + currentDirectory);
+		table = Parser.loadCSVTable("test1.csv");
+
+//		// Create grid pane
+//		gridPane = GUI.populateGrid(table);
+//		ScrollPane sp = new ScrollPane(gridPane);
+//
+//		// Create a button for saving the table
+//		Button dodajredbtn = new Button("dodaj red");
+//		dodajredbtn.setOnAction(e -> cont.dodajRed(gridPane));
+//
+//		Button printbtn = new Button("stampaj");
+//		// printbtn.setOnAction(e -> cont.stampaj(gridPane));
+//		printbtn.setOnAction(e -> {
+//			sp.setContent(GUI.populateGrid(table));
+//		});
+//
+//		TextField red = new TextField();
+//		red.setPromptText("red");
+//		TextField kolona = new TextField();
+//		kolona.setPromptText("kolona");
+//		TextField sadrzaj = new TextField();
+//		sadrzaj.setPromptText("sadrzaj");
+//
+//		Button dodajpolje = new Button("dodaj polje");
+//		dodajpolje.setOnAction(e -> cont.dodajPolje(Integer.parseInt(red.getText()), Integer.parseInt(kolona.getText()),
+//				new Cell(sadrzaj.getText())));
+//
+//		Button replacebtn = new Button("replace");
+//		replacebtn.setOnAction(e -> {
+//			Label l = new Label("zamenjeno");
+//			replaceElementInGrid(gridPane, l, Integer.parseInt(red.getText()), Integer.parseInt(kolona.getText()));
+//		});
+//
+//		// Create a VBox to hold the save button
+//		VBox buttonBox = new VBox(10);
+//		buttonBox.getChildren().add(dodajredbtn);
+//		buttonBox.getChildren().add(printbtn);
+//		buttonBox.getChildren().addAll(red, kolona, sadrzaj);
+//		buttonBox.getChildren().add(dodajpolje);
+//		buttonBox.getChildren().add(replacebtn);
+//		buttonBox.setPadding(new Insets(10));
+//
+//		// Add the VBox to the right side of the main layout
+//		root.setRight(buttonBox);
+//		root.setCenter(sp);
 
 		// Create the scene and set it on the stage
-		Scene scene = new Scene(root, 800, 600);
-		primaryStage.setScene(scene);
+		primaryStage.setScene(makeAndPopulateScene());
 		primaryStage.show();
 	}
 
--- poop_projekat_java/src/application/Parser.java
+++ poop_projekat_java/src/application/Parser.java
@@ -3,7 +3,6 @@ package application;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
-import java.io.IOException;
 import java.nio.file.Paths;
 
 public class Parser {
